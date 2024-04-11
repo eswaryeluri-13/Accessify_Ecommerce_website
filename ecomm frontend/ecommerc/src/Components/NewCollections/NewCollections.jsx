a79caf32b39da040ec8f8db8c415a8688f6aea3d
Eswar Yeluri <eswaryeluri13@gmail.com>
2024-04-11 13:16:13 +0530

    Updated build and css

--- ecomm frontend/ecommerc/src/Components/NewCollections/NewCollections.jsx
+++ ecomm frontend/ecommerc/src/Components/NewCollections/NewCollections.jsx
@@ -1,29 +1,37 @@
-import React,{useEffect,useState} from 'react'
-import './NewCollections.css'
+import React, { useEffect, useState } from "react";
+import "./NewCollections.css";
 //import new_collection from '../Assets/new_collections'
-import Item from '../Item/Item'
+import Item from "../Item/Item";
 
 function NewCollections() {
+  const [new_collection, setNew_collection] = useState([]);
 
-  const [new_collection,setNew_collection]=useState([]);
-
-  useEffect(()=>{
-     fetch('http://localhost:4000/newcollections').then((response)=>response.json()).then((data)=>setNew_collection(data));
-  },[])
+  useEffect(() => {
+    fetch("https://accessify-ecommerce-website.onrender.com/newcollections")
+      .then((response) => response.json())
+      .then((data) => setNew_collection(data));
+  }, []);
 
   return (
-    <div className='new-collections'>
-        <h1>NEW COLLECTIONS</h1>
-        <hr/>
-        <div className='collections'>
-            {new_collection.map((item,i)=>{
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>;
-            })}
-
-        </div>
-
+    <div className="new-collections">
+      <h1>NEW COLLECTIONS</h1>
+      <hr />
+      <div className="collections">
+        {new_collection.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default NewCollections
+export default NewCollections;
--- ecomm frontend/ecommerc/src/Context/ShopContext.jsx
+++ ecomm frontend/ecommerc/src/Context/ShopContext.jsx
@@ -1,102 +1,113 @@
-import React,{ createContext ,useEffect,useState}from "react"
+import React, { createContext, useEffect, useState } from "react";
 //import all_product from "../Components/Assets/all_product";
 //import Shop from "../pages/Shop";
 
 export const ShopContext = createContext(null);
 
+const getDefaultCart = () => {
+  let cart = {};
+  for (let index = 0; index < 300 + 1; index++) {
+    cart[index] = 0;
+  }
+  return cart;
+};
 
-const getDefaultCart=()=>{
-    let cart={};
-    for(let index=0;index<300+1;index++)
-    {
-        cart[index]=0;
-    }
-    return cart;
-}
+const ShopContextProvider = (props) => {
+  const [all_product, setAll_Product] = useState([]);
+  const [cartItems, setCartItems] = useState(
+    getDefaultCart()
+  ); /*variable and function , function*/
 
-const ShopContextProvider = (props)=>{
+  useEffect(() => {
+    fetch("https://accessify-ecommerce-website.onrender.com/allproducts")
+      .then((response) => response.json())
+      .then((data) => setAll_Product(data));
 
-    const [all_product,setAll_Product]=useState([]);
-    const [cartItems,setCartItems]=useState(getDefaultCart())/*variable and function , function*/
-    
-    useEffect(()=>{
-        fetch('http://localhost:4000/allproducts').then((response)=>response.json()).then((data)=>setAll_Product(data))
-
-        if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/getcart',{
-                method:'POST',
-                headers:{
-                    Accept:'application/form-data',
-                    'auth-token':`${localStorage.getItem('auth-token')}`,
-                    'Content-Type':'application/json',
-                },
-                body:"",
-            }).then((response)=>response.json()).then((data)=>setCartItems(data));
-        }
-    },[])
-    
-    const addToCart=(itemId)=>{
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
-        if(localStorage.getItem('auth-token')) {
-            fetch('http://localhost:4000/addtocart',{
-            method: 'POST' ,
-            headers :{
-            Accept:'application/form-data',
-            'auth-token' : `${localStorage.getItem('auth-token')}`,
-            'Content-Type' :'application/json',
+    if (localStorage.getItem("auth-token")) {
+      fetch("https://accessify-ecommerce-website.onrender.com/getcart", {
+        method: "POST",
+        headers: {
+          Accept: "application/form-data",
+          "auth-token": `${localStorage.getItem("auth-token")}`,
+          "Content-Type": "application/json",
         },
-        body:JSON.stringify({"itemID":itemId}),
-    }).then((response)=>response.json()).then((data)=>console.log(data));
+        body: "",
+      })
+        .then((response) => response.json())
+        .then((data) => setCartItems(data));
     }
-}
-    const removeFromCart=(itemId)=>{
-        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}));
-        if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/removefromcart',{
-                method: 'POST' ,
-                headers :{
-                Accept:'application/form-data',
-                'auth-token' : `${localStorage.getItem('auth-token')}`,
-                'Content-Type' :'application/json',
-            },
-            body:JSON.stringify({"itemID":itemId}),
-        }).then((response)=>response.json()).then((data)=>console.log(data));
-        }
+  }, []);
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    if (localStorage.getItem("auth-token")) {
+      fetch("https://accessify-ecommerce-website.onrender.com/addtocart", {
+        method: "POST",
+        headers: {
+          Accept: "application/form-data",
+          "auth-token": `${localStorage.getItem("auth-token")}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ itemID: itemId }),
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data));
     }
-
-    const getTotalCartAmount=()=>{
-        let totalAmount=0;
-        for(const item in cartItems)
-        {
-            if(cartItems[item]>0)
-            {
-                let itemInfo = all_product.find((product)=>product.id===Number(item));
-                totalAmount+=itemInfo.new_price*cartItems[item];
-            }
-        }
-        return totalAmount;
+  };
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    if (localStorage.getItem("auth-token")) {
+      fetch("https://accessify-ecommerce-website.onrender.com/removefromcart", {
+        method: "POST",
+        headers: {
+          Accept: "application/form-data",
+          "auth-token": `${localStorage.getItem("auth-token")}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ itemID: itemId }),
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data));
     }
-    
-    const getTotalCartItems=()=>{
-        let totalItem=0;
-        for(const item in cartItems)
-        {
-            if(cartItems[item]>0)
-            {
-                totalItem+=cartItems[item];
-            }
-        }
-        return totalItem;
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_product.find(
+          (product) => product.id === Number(item)
+        );
+        totalAmount += itemInfo.new_price * cartItems[item];
+      }
     }
+    return totalAmount;
+  };
 
+  const getTotalCartItems = () => {
+    let totalItem = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItem += cartItems[item];
+      }
+    }
+    return totalItem;
+  };
 
-    const contextValue={getTotalCartItems,getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart}
+  const contextValue = {
+    getTotalCartItems,
+    getTotalCartAmount,
+    all_product,
+    cartItems,
+    addToCart,
+    removeFromCart,
+  };
 
-    return (
-        <ShopContext.Provider value={contextValue}>
-            {props.children}
-        </ShopContext.Provider>
-    )
-}
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
 
 export default ShopContextProvider;
